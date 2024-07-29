38f467a93d105fcf60e5422bf1df7e3d36de8467
Zubaha <zubaha4052@gmail.com>
2024-07-29 20:08:11 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import userData from "./data/userData.json";
+import friends from "./data/friends.json";
+import items from "./data/transactions.json";
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={items} />
+    </>
+  );
+};
+
+export default App;
